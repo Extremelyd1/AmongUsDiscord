d7ba1593a60620e86c8268733d4a3df6dc0bcb15
Extremelyd1 <Extremelyd1@outlook.com>
2020-09-18 23:08:51 +0200

    Updated Among Us program finding and readme
    Also prematurely bumped version number

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -1,25 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
 using Microsoft.Win32;
 
 namespace Launcher {
     internal class Program {
-        private static readonly string VERSION_FILE_URL = "https://extremelyd1.github.io/AmongUsDiscord/latest.txt";
+        private const string VersionFileUrl = "https://extremelyd1.github.io/AmongUsDiscord/latest.txt";
 
-        private static readonly string REGISTRY_STEAM_INSTALL_PATH_PATH = 
-            "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Valve\\Steam";
+        private const string RegistrySteamInstallPathPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Valve\\Steam";
 
-        private static readonly string REGISTRY_STEAM_INSTALL_PATH_KEY_NAME = "InstallPath";
+        private const string RegistrySteamInstallPathKeyName = "InstallPath";
 
-        private static readonly string AMONG_US_PATH = "\\steamapps\\common\\Among Us\\";
+        private const string AmongUsManifestPath = "\\steamapps\\appmanifest_945360.acf";
+
+        private const string LibraryFoldersPath = "\\steamapps\\libraryfolders.vdf";
         
-        private static readonly string DISCORD_EXECUTABLE_NAME = "AmongUsDiscordIntegration.exe";
-        
-        private static readonly string AMONG_US_EXECUTABLE_NAME = "Among Us.exe";
+        private const string AmongUsPath = "\\steamapps\\common\\Among Us\\";
 
-        private bool _dev;
+        private const string DiscordExecutableName = "AmongUsDiscordIntegration.exe";
+
+        private const string AmongUsExecutableName = "Among Us.exe";
+
+        private readonly bool _dev;
 
         public static void Main(string[] args) {
             if (args.Length < 1) {
@@ -43,22 +49,29 @@ namespace Launcher {
             }
 
             Console.WriteLine("Finding steam directory path...");
-            
-            string steamDirectoryPath = GetSteamDirectoryPath();
+
+            if (!GetAmongUsSteamLibraryPath(out var amongUsSteamDirectoryPath)) {
+                Console.WriteLine("Could not find Among Us installation path");
+                Console.WriteLine("Exiting in 5 seconds...");
+                
+                Thread.Sleep(5000);
+                
+                return;
+            }
             
             Console.WriteLine("Checking for updates...");
             
-            string latestReleaseUrl = GetLatestReleaseUrl();
+            var latestReleaseUrl = GetLatestReleaseUrl();
             
-            string filePath = steamDirectoryPath + AMONG_US_PATH;
+            var filePath = amongUsSteamDirectoryPath + AmongUsPath;
 
             if (!_dev) {
                 // Check whether discord integration file already exists
-                if (File.Exists(filePath + DISCORD_EXECUTABLE_NAME)) {
-                    File.Delete(filePath + DISCORD_EXECUTABLE_NAME);
+                if (File.Exists(filePath + DiscordExecutableName)) {
+                    File.Delete(filePath + DiscordExecutableName);
                 }
 
-                DownloadIntegration(latestReleaseUrl, filePath + DISCORD_EXECUTABLE_NAME);
+                DownloadIntegration(latestReleaseUrl, filePath + DiscordExecutableName);
             }
 
             // Check if game is not running yet
@@ -66,7 +79,7 @@ namespace Launcher {
                 Console.WriteLine("Launching game...");
 
                 // Launch Among Us
-                Process.Start(filePath + AMONG_US_EXECUTABLE_NAME);
+                Process.Start(filePath + AmongUsExecutableName);
             } else {
                 Console.WriteLine("Game is already running...");
             }
@@ -74,29 +87,84 @@ namespace Launcher {
             Console.WriteLine("Launching discord integration...");
             
             // Launch Among Us Discord
-            Process.Start(filePath + DISCORD_EXECUTABLE_NAME);
+            Process.Start(filePath + DiscordExecutableName);
 
             Console.WriteLine("Exiting...");
         }
 
         private string GetLatestReleaseUrl() {
             using (var client = new WebClient()) {
-                return client.DownloadString(VERSION_FILE_URL);
+                return client.DownloadString(VersionFileUrl);
             }
         }
 
-        private string GetSteamDirectoryPath() {
+        private bool GetAmongUsSteamLibraryPath(out string amongUsSteamLibraryPath) {
+            amongUsSteamLibraryPath = "";
+            
+            var mainSteamDirectoryPath = GetMainSteamDirectoryPath();
+
+            // If the manifest file for Among Us resides in the main steam directory
+            // we can immediately return it
+            if (File.Exists(mainSteamDirectoryPath + AmongUsManifestPath)) {
+                amongUsSteamLibraryPath = mainSteamDirectoryPath;
+
+                return true;
+            }
+            
+            // Otherwise we have to search for the Steam Library in which Among Us is stored
+            var libraryFoldersContent = ReadFileToString(mainSteamDirectoryPath + LibraryFoldersPath);
+
+            if (!ParseLibraryFoldersFile(libraryFoldersContent, out var steamLibraryPaths)) {
+                return false;
+            }
+
+            foreach (var path in steamLibraryPaths) {
+                if (File.Exists(path + AmongUsManifestPath)) {
+                    amongUsSteamLibraryPath = path;
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private string GetMainSteamDirectoryPath() {
             return (string) Registry.GetValue(
-                REGISTRY_STEAM_INSTALL_PATH_PATH, 
-                REGISTRY_STEAM_INSTALL_PATH_KEY_NAME,
+                RegistrySteamInstallPathPath, 
+                RegistrySteamInstallPathKeyName,
                 ""
             );
+        }
+
+        private bool ParseLibraryFoldersFile(string libraryFoldersContent, out List<string> steamLibraryPaths) {
+            steamLibraryPaths = new List<string>();
+
+            // Remove all spaces and tabs from the content
+            libraryFoldersContent = Regex.Replace(libraryFoldersContent, "[\t ]", "");
+
+            // Split on " character
+            var libraryFoldersContentSplit = libraryFoldersContent.Split('\"');
+
+            if (libraryFoldersContentSplit.Length < 15) {
+                return false;
+            }
+
+            for (var i = 13; i < libraryFoldersContentSplit.Length; i += 4) {
+                steamLibraryPaths.Add(libraryFoldersContentSplit[i]);
+            }
+
+            return true;
         }
 
         private void DownloadIntegration(string url, string downloadFile) {
             using (var client = new WebClient()) {
                 client.DownloadFile(url, downloadFile);
             }
+        }
+
+        private string ReadFileToString(string path) {
+            return File.ReadAllText(path);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # Among Us Discord
-## A tool to handle discord muting/deafening
-Readme WIP
+A tool to handle discord muting/deafening for Among Us
+
+## What is Among Us Discord
+Among Us Discord is a user-application that runs on your computer.
+It's goal is to provide an immersive experience when playing with a group that is in a Discord call.
+It accomplishes this by muting/deafening the user at appropriate moments.
+Namely, all users are muted and deafened at the start of the game.
+Users are only unmuted and undeafened if they are still alive when a meeting is called.
+After a meeting, all alive players are muted and deafened again to allow dead players to speak among themselves.
+
+## Usage
+This tool only works if you run Discord and the game Among Us on the same device.
+More specifically, this program is made for Windows, thus mobile Discord/Among Us users are not supported.
+- Go to the [releases page](https://github.com/Extremelyd1/AmongUsDiscord/releases) and download the latest `Launcher.exe`.
+- Go to Discord -> User Settings -> Keybinds, and create new keybinds for the following:
+    - A keybind with Action `Toggle Mute` on keys `CTRL + F1`
+    - A keybind with Action `Toggle Deafen` on keys `CTRL + F1`
+- Run the `Launcher.exe` (with administrative rights if asked), and voila!
+ 
+The program will automatically launch Among Us if it is not running yet. 
+ 
+Note that the program might not run if you don't have `.NETFramework v4.7.2` installed. If the program does not start or gives an error, please check this first.
+
+## Technical details
+### Among Us directory location
+The launcher automatically finds the install location of Among Us by finding the main Steam install path from registry.
+This main Steam Library contains a file with entries pertaining to all registered Steam Library locations on disk. 
+The launcher then checks each of these Library locations for the existence of the Among Us directory.
+
+### Memory Reading
+The way the program is able to distinguish game states and mute/deafen accordingly is by reading the process memory.
+The structs outlined in `AmongUsDiscordIntegration/Structs` are exactly stored in this way in memory. 
+By finding a stable pointer (or pointer path) to the start of these structs in the process memory of Among Us, we can mimic their structure with values in the program.
